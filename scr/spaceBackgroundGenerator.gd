ebe7b094a0273e84c68f8bd99de36dee8fdfe813
frozy <92292093+Pentannex-Studios@users.noreply.github.com>
2023-07-10 15:21:57 +0800

    071023 | Reworked Space Generation Code
    Introduced new framework on space generation.
    - Limited the usage of exclusive methods for main thread in secondary threads (such as get_node() functions) as it is not thread-safe and can lead to unpredictable outcomes.
    - The framework before was assign the textures in nodes AFTER it was created and configured which freezes the main thread. Build #071023 reworks the generation by creating the textures and assigning it BEFORE configuration (Creating NoiseTexture2D doesn't freeze the main thread, only when configured it with noise and color_ramps which the secondary thread will be responsible processing).
    - Rewritten the whole code with new conventions and make it much more clear and concise. Limiting the repetitive amounts of duplicate code.

--- scr/spaceBackgroundGenerator.gd
+++ scr/spaceBackgroundGenerator.gd
@@ -1,9 +1,11 @@
 extends Node2D
 #------------------------------------------------------------------------------#
-@onready var spaceParallaxManager: ParallaxBackground = get_node("spaceParallaxEffectManager")
+var _spaceGenerationThread: Thread
 
-#------------------------------------------------------------------------------#
-var spaceGenerationThread: Thread
+var _spaceSectorKey: String = "[|{0}|{1}|{2}|{3}|{4}|{5}|]"
+
+# Initiation of textures.
+var _spaceAssetsIndex: int = 1
 
 #------------------------------------------------------------------------------#
 # Default space generation on start (Without "spaceSectorManager.tscn", scene manager). 
@@ -15,192 +17,139 @@ func _ready() -> void:
 	if lockDefault or get_parent() == get_tree().root:
 		initiateThreadForLoading(
 			[
-			lib.generateRandomNumber(lib.spaceMinimumSpawnSeed, lib.spaceMaximumSpawnSeed, "int", true), 
-			lib.generateRandomColor(lib.spaceGasesMinimumColorValue, lib.spaceGasesMaximumColorValue),
-			lib.generateRandomColor(lib.spaceGasesMinimumColorValue, lib.spaceGasesMaximumColorValue),
-			lib.generateRandomColor(lib.spaceGasesMinimumColorValue, lib.spaceGasesMaximumColorValue),
+				[
+					lib.generateRandomNumber(lib.spaceMinimumSpawnSeed, lib.spaceMaximumSpawnSeed, "int", true), 
+					lib.generateRandomColor(),
+					lib.generateRandomColor(),
+					lib.generateRandomColor(),
+					lib.generateRandomColor(),
+					lib.generateRandomNumber(1, 2)
+				]
 			]
 		)
 
 #------------------------------------------------------------------------------#
 # IMPORTANT CODE: Load a thread for facilitating background loading
 func initiateThreadForLoading(spaceArray: Array) -> void:
-	
-	# Wait for enough time to prepare nodes for texture placement.
-	await get_tree().create_timer(5).timeout
-	
 	# IGNORE: Debug
 	print("\nCreating thread for background texture loading...")
 	# IMPORTANT CODE: Checking if other "spaceGenerationThreads" are active
-	if is_instance_valid(spaceGenerationThread) and spaceGenerationThread.is_started():
-		spaceGenerationThread.wait_to_finish()
+	if is_instance_valid(_spaceGenerationThread) and _spaceGenerationThread.is_started():
+		_spaceGenerationThread.wait_to_finish()
 	
 	# Create a new thread.
-	spaceGenerationThread = Thread.new()
-	
+	_spaceGenerationThread = Thread.new()
 	# IGNORE: Debug
-	print("Thread ", spaceGenerationThread, " has been created!\n")
-	print("Loading space background textures in the thread...")
+	print("Thread ", _spaceGenerationThread, " has been created!\n")
+	
+	# Initiation of textures. Loading the texture will be done in the thread.
+	var _spaceBackgroundTexture: NoiseTexture2D = NoiseTexture2D.new()
+	var _spaceGasesTexture: NoiseTexture2D = NoiseTexture2D.new()
+	var _spaceGases1Texture: NoiseTexture2D = NoiseTexture2D.new()
+	var _spaceGases2Texture: NoiseTexture2D = NoiseTexture2D.new()
+	var _spaceStarsTexture: NoiseTexture2D = NoiseTexture2D.new()
+	var _spaceStars1Texture: NoiseTexture2D = NoiseTexture2D.new()
+	
+	get_node("spaceParallaxEffectManager/spaceBGTParallax/spaceBackgroundTexture").texture = _spaceBackgroundTexture
+	get_node("spaceParallaxEffectManager/spaceGTParallax/spaceGasesTexture").texture = _spaceGasesTexture
+	get_node("spaceParallaxEffectManager/spaceGT1Parallax/spaceGasesTexture1").texture = _spaceGases1Texture
+	get_node("spaceParallaxEffectManager/spaceGT2Parallax/spaceGasesTexture2").texture = _spaceGases2Texture
+	get_node("spaceParallaxEffectManager/spaceSTParallax/spaceStarTexture").texture = _spaceStarsTexture
+	get_node("spaceParallaxEffectManager/spaceST1Parallax/spaceStarTexture1").texture = _spaceStars1Texture
+	
+	get_node("spaceParallaxEffectManager/spaceGTParallax/spaceGasesTexture").modulate = spaceArray[0][2]
+	get_node("spaceParallaxEffectManager/spaceGT1Parallax/spaceGasesTexture1").modulate = spaceArray[0][3]
+	get_node("spaceParallaxEffectManager/spaceGT2Parallax/spaceGasesTexture2").modulate = spaceArray[0][4]
+	
+	spaceArray.append([_spaceBackgroundTexture, _spaceGasesTexture, _spaceGases1Texture, _spaceGases2Texture, _spaceStarsTexture, _spaceStars1Texture])
 	
 	# Proceed to the thread function
-	spaceGenerationThread.start(_initiateSceneLoadingInThread.bind(spaceArray))
+	_spaceGenerationThread.start(_initiateSceneLoadingInThread.bind(spaceArray))
+	# IGNORE: Debug
+	print("Printing array contents to be sent in thread ", _spaceGenerationThread, ": ", spaceArray)
+	# IGNORE: Debug
+	print("\nLoading space background textures in the thread...")
 
 # IMPORTANT CODE: Thread Function for background space generation. 
 func _initiateSceneLoadingInThread(spaceArray: Array) -> void:
 	# IGNORE: Debug
 	print("\nThread function initiated!\n")
-	
-	# Assign the color to the textures. 
-	get_node("spaceParallaxEffectManager/spaceGTParallax/spaceGasesTexture").modulate = spaceArray[1]
-	get_node("spaceParallaxEffectManager/spaceGT1Parallax/spaceGasesTexture1").modulate = spaceArray[2]
-	get_node("spaceParallaxEffectManager/spaceGT2Parallax/spaceGasesTexture2").modulate = spaceArray[3]
-	
-	# Initiation of textures.
-	var spaceAssetBackground: NoiseTexture2D = NoiseTexture2D.new()
-	var spaceAssetGases: NoiseTexture2D = NoiseTexture2D.new()
-	var spaceAssetGases1: NoiseTexture2D = NoiseTexture2D.new()
-	var spaceAssetGases2: NoiseTexture2D = NoiseTexture2D.new()
-	var spaceAssetStars: NoiseTexture2D = NoiseTexture2D.new()
-	var spaceAssetStars1: NoiseTexture2D = NoiseTexture2D.new()
-	
-	# Put in array to use in loop.
-	var spaceAssets: Array = [spaceAssetBackground, spaceAssetGases, spaceAssetGases1, spaceAssetGases2, spaceAssetStars, spaceAssetStars1]
-	var spaceAssetsIndex: int = 1
-	
+
 	# Loop basic information to the textures.
-	for spaceAssetBasicGeneration in spaceAssets:
-		
+	for _spaceTextureGenerationIteration in range(6):
 		# Creating the default configuration of the textures.
-		spaceAssetBasicGeneration.width = 4096
-		spaceAssetBasicGeneration.height = 4096
-		spaceAssetBasicGeneration.generate_mipmaps = false
-#		spaceAssetBasicGeneration.normalize = false
-		
+		spaceArray[1][_spaceTextureGenerationIteration].width = 4096
+		spaceArray[1][_spaceTextureGenerationIteration].height = 4096
+		spaceArray[1][_spaceTextureGenerationIteration].generate_mipmaps = false
 		# IGNORE: Debug
-		print("Generated a space texture! ", spaceAssetBasicGeneration)
+		print("Generated a space texture! ", spaceArray[1][_spaceTextureGenerationIteration])
 		
 		# Creates a standard color ramp and noise object to assign in the texture.
-		var spaceAssetBasicGenerationColorRamp: Gradient = Gradient.new()
-		spaceAssetBasicGenerationColorRamp.offsets = [0.5, 1]
-		spaceAssetBasicGenerationColorRamp.colors = [Color.TRANSPARENT, Color.WHITE]
-		spaceAssetBasicGenerationColorRamp.interpolation_mode = Gradient.GRADIENT_INTERPOLATE_CUBIC
+		var _spaceTextureColorRamp: Gradient = Gradient.new()
+		spaceArray[1][_spaceTextureGenerationIteration].color_ramp = _spaceTextureColorRamp
+		_spaceTextureColorRamp.offsets = [0.5, 1]
+		_spaceTextureColorRamp.colors = [Color.TRANSPARENT, Color.WHITE]
+		_spaceTextureColorRamp.interpolation_mode = Gradient.GRADIENT_INTERPOLATE_CUBIC
+		# IGNORE: Debug
+		print("Generated color ramp! ", _spaceTextureColorRamp)
 		
-		var spaceAssetBasicGenerationNoise: FastNoiseLite = FastNoiseLite.new()
-		spaceAssetBasicGenerationNoise.seed = spaceArray[0]
-		spaceAssetBasicGenerationNoise.fractal_octaves = 10
+		var _spaceTextureNoise: FastNoiseLite = FastNoiseLite.new()
+		spaceArray[1][_spaceTextureGenerationIteration].noise = _spaceTextureNoise
+		_spaceTextureNoise.seed = spaceArray[0][0]
+		_spaceTextureNoise.fractal_octaves = 10
+		# IGNORE: Debug
+		print("Generated noise! ", _spaceTextureNoise)
 		
 		# Configure the adjustments per texture.
-		match spaceAssetsIndex:
-			1:  # Space background generation.
-				spaceAssetBasicGenerationNoise.noise_type = FastNoiseLite.TYPE_SIMPLEX_SMOOTH
-				spaceAssetBasicGenerationColorRamp.offsets = [0, 1]
-				spaceAssetBasicGenerationColorRamp.colors = [Color.BLACK, Color.WHITE]
-				
-				# Assign the generated ramp and noise into the texture and to the node.
-				spaceAssetBasicGeneration.color_ramp = spaceAssetBasicGenerationColorRamp
-				spaceAssetBasicGeneration.noise = spaceAssetBasicGenerationNoise
-				get_node("spaceParallaxEffectManager/spaceBGTParallax/spaceBackgroundTexture").texture = spaceAssetBackground
-				
-				# Increase index by 1 to determine what type of adjustments to add in the textures.
-				spaceAssetsIndex += 1
-			
-			2:  # Space gas generation.
-				# Decide to show texture or not.
-				if lib.generateRandomNumber(0, 1, "int", false) == 1:
-					spaceAssetBasicGenerationNoise.noise_type = FastNoiseLite.TYPE_SIMPLEX_SMOOTH
-					
-					# Assign the generated ramp and noise into the texture and to the node.
-					spaceAssetBasicGeneration.color_ramp = spaceAssetBasicGenerationColorRamp
-					spaceAssetBasicGeneration.noise = spaceAssetBasicGenerationNoise
-					get_node("spaceParallaxEffectManager/spaceGTParallax/spaceGasesTexture").texture = spaceAssetGases
-				
-				else:
-					spaceAssetGases = null
-					get_node("spaceParallaxEffectManager/spaceGTParallax/spaceGasesTexture").visible = false
-					
-					# IGNORE: Debug
-					print("Texture ", spaceAssetBasicGeneration, " is hidden.")
-				
-				# Increase index by 1 to determine what type of adjustments to add in the textures.
-				spaceAssetsIndex += 1
-			
-			3:  # Space gas 1 generation.
-				# Decide to show texture or not.
-				if lib.generateRandomNumber(0, 1, "int", false) == 1:
-					spaceAssetBasicGenerationNoise.noise_type = FastNoiseLite.TYPE_PERLIN
-					
-					# Assign the generated ramp and noise into the texture and to the node.
-					spaceAssetBasicGeneration.color_ramp = spaceAssetBasicGenerationColorRamp
-					spaceAssetBasicGeneration.noise = spaceAssetBasicGenerationNoise
-					get_node("spaceParallaxEffectManager/spaceGT1Parallax/spaceGasesTexture1").texture = spaceAssetGases1
-				
-				else:
-					spaceAssetGases1 = null
-					get_node("spaceParallaxEffectManager/spaceGT1Parallax/spaceGasesTexture1").visible = false
-					
-					# IGNORE: Debug
-					print("Hidden ", spaceAssetBasicGeneration, " texture.")
-				
-				# Increase index by 1 to determine what type of adjustments to add in the textures.
-				spaceAssetsIndex += 1
-			
-			4:  # Space gas 2 generation.
-				# Decide to show texture or not.
-				if lib.generateRandomNumber(0, 1, "int", false) == 1:
-					spaceAssetBasicGenerationNoise.noise_type = FastNoiseLite.TYPE_PERLIN
-					spaceAssetBasicGenerationColorRamp.offsets = [0.25, 1]
-				
-					# Assign the generated ramp and noise into the texture and to the node.
-					spaceAssetBasicGeneration.color_ramp = spaceAssetBasicGenerationColorRamp
-					spaceAssetBasicGeneration.noise = spaceAssetBasicGenerationNoise
-					get_node("spaceParallaxEffectManager/spaceGT2Parallax/spaceGasesTexture2").texture = spaceAssetGases2
-				
-				else:
-					spaceAssetGases2 = null
-					get_node("spaceParallaxEffectManager/spaceGT2Parallax/spaceGasesTexture2").visible = false
-					
-					# IGNORE: Debug
-					print("Texture ", spaceAssetBasicGeneration, " is hidden.")
-				
-				# Increase index by 1 to determine what type of adjustments to add in the textures.
-				spaceAssetsIndex += 1
-			
-			5:  # Space star generation.
-				spaceAssetBasicGenerationNoise.noise_type = FastNoiseLite.TYPE_SIMPLEX_SMOOTH
-				spaceAssetBasicGenerationColorRamp.offsets = [0.85, 1]
-				spaceAssetBasicGenerationNoise.frequency = 0.15
-				spaceAssetBasicGenerationNoise.fractal_octaves = 5
-				spaceAssetBasicGenerationNoise.fractal_gain = 2
-				
-				# Assign the generated ramp and noise into the texture and to the node.
-				spaceAssetBasicGeneration.color_ramp = spaceAssetBasicGenerationColorRamp
-				spaceAssetBasicGeneration.noise = spaceAssetBasicGenerationNoise
-				get_node("spaceParallaxEffectManager/spaceSTParallax/spaceStarTexture").texture = spaceAssetStars
-				
-				# Increase index by 1 to determine what type of adjustments to add in the textures.
-				spaceAssetsIndex += 1
-			
-			6:  # Space star 1 generation.
-				spaceAssetBasicGenerationNoise.noise_type = FastNoiseLite.TYPE_SIMPLEX_SMOOTH
-				spaceAssetBasicGenerationColorRamp.offsets = [0.80, 1]
-				spaceAssetBasicGenerationNoise.frequency = 0.15
-				spaceAssetBasicGenerationNoise.fractal_octaves = 5
-				spaceAssetBasicGenerationNoise.fractal_gain = 2
-				
-				# Assign the generated ramp and noise into the texture and to the node.
-				spaceAssetBasicGeneration.color_ramp = spaceAssetBasicGenerationColorRamp
-				spaceAssetBasicGeneration.noise = spaceAssetBasicGenerationNoise
-				get_node("spaceParallaxEffectManager/spaceST1Parallax/spaceStarTexture1").texture = spaceAssetStars1
-				
-				# Increase index by 1 to determine what type of adjustments to add in the textures.
-				spaceAssetsIndex += 1
-		
-		# IGNORE: Debug
-		print("Adjusted space texture! ", spaceAssetBasicGeneration, " Index: ", spaceAssetsIndex - 1)
-		print("Adjustments (Offsets, Colors, Mode): ", spaceAssetBasicGenerationColorRamp.offsets, " ", spaceAssetBasicGenerationColorRamp.colors, " ", spaceAssetBasicGenerationColorRamp.interpolation_mode)
-		print("Adjustments (Seed): ", spaceAssetBasicGenerationNoise.seed, " ",  "\n")
+		# IMPORTANT CODE: Print statements per index is necessary for debugging and MUST NOT be removed.
+		# IMPORTANT CODE: It checks for what texture has failed loading as threads don't receive error messages.
+		if _spaceAssetsIndex == 1:
+			# Space background generation.
+			_spaceTextureNoise.noise_type = FastNoiseLite.TYPE_SIMPLEX_SMOOTH
+			_spaceTextureColorRamp.offsets = [0.5, 1]
+			_spaceTextureColorRamp.colors = [Color("1a001a"), spaceArray[0][1]]
+			# IGNORE: Debug
+			print("Adjusted space background texture! ", spaceArray[1][_spaceTextureGenerationIteration], "\n")
+		elif _spaceAssetsIndex in range(2, 5):  
+			# Space gases generation.
+			# Decide to show texture or not.
+			if spaceArray[0][_spaceAssetsIndex] != "00000000":
+				# Adjusts texture based on index.
+				if _spaceAssetsIndex == 2:
+					_spaceTextureNoise.noise_type = FastNoiseLite.TYPE_SIMPLEX_SMOOTH
+				elif _spaceAssetsIndex == 3: 
+					_spaceTextureNoise.noise_type = FastNoiseLite.TYPE_PERLIN
+				elif _spaceAssetsIndex == 4:
+					_spaceTextureNoise.noise_type = FastNoiseLite.TYPE_PERLIN
+					_spaceTextureColorRamp.offsets = [0.25, 1]
+			else:
+				# Removes the texture if texture is not visible to save memory.
+				var _spaceTextureToBeRemoved = spaceArray[1][_spaceTextureGenerationIteration]
+				spaceArray[1][_spaceTextureGenerationIteration] = 0
+				_spaceTextureToBeRemoved.queue_free()
+				# IGNORE: Debug
+				print("Queue Free'd' a texture because modulate is 0.", spaceArray[1][_spaceTextureGenerationIteration])
+			print("Adjusted a space gas texture! ", spaceArray[1][_spaceTextureGenerationIteration], "Index: ", _spaceAssetsIndex, "\n")
+		elif _spaceAssetsIndex > 4: 
+			# Space star generation.
+			_spaceTextureNoise.noise_type = FastNoiseLite.TYPE_SIMPLEX_SMOOTH
+			_spaceTextureNoise.frequency = 0.15
+			_spaceTextureNoise.fractal_octaves = 5
+			_spaceTextureNoise.fractal_gain = 2
+			if _spaceAssetsIndex == 5:
+				_spaceTextureColorRamp.offsets = [0.85, 1]
+				# IGNORE: Debug
+				print("Adjusted a space star texture! ", spaceArray[1][_spaceTextureGenerationIteration], "\n")
+			elif _spaceAssetsIndex == 6:
+				_spaceTextureColorRamp.offsets = [0.80, 1]
+				# IGNORE: Debug
+				print("Adjusted a space star texture! ", spaceArray[1][_spaceTextureGenerationIteration], "\n")
+		# Increase index by 1 to determine what type of adjustments to add in the textures.
+		_spaceAssetsIndex += 1
 	
 	# IGNORE: Debug
-	print("Loaded space texture! Visually adjusting now. Thread ", spaceGenerationThread, " will be terminated.")
+	print("Loaded space sector! Visually adjusting now. Thread ", _spaceGenerationThread, " will be terminated.")
+	print("Generated Space Sector Key (SSK): ", _spaceSectorKey.format(spaceArray[0]))
+	print("#------------------------------------------------------------------------------#")
 
 #------------------------------------------------------------------------------#
--- scr/spaceSectorManager.gd
+++ scr/spaceSectorManager.gd
@@ -1,16 +1,16 @@
 extends Node2D
 #------------------------------------------------------------------------------#
-@onready var spaceBackgroundGenerator: Node2D = get_node("spaceBackgroundGenerator")
-@onready var spacePhenomenaGenerator: Node2D = get_node("spacePhenomenaGenerator")
-@onready var spaceEnvironment: WorldEnvironment = get_node("spaceEnvironment")
+@onready var _spaceBackgroundGenerator: Node2D = get_node("spaceBackgroundGenerator")
+@onready var _spacePhenomenaGenerator: Node2D = get_node("spacePhenomenaGenerator")
+#@onready var _spaceEnvironment: WorldEnvironment = get_node("spaceEnvironment")
 
 #------------------------------------------------------------------------------#
-var spaceSectorSeed: int
-var spaceSectorGases: Array = []
-var spaceSectorSize: Vector2
+var _spaceSectorRandomGeneration: bool = false
 
-
-var spaceSectorPhenomena: int
+var _spaceSectorSeed: int
+var _spaceSectorBackground: String
+var _spaceSectorGases: Array = []
+var _spaceSectorPhenomena: int
 
 #------------------------------------------------------------------------------#
 func _ready() -> void:
@@ -20,36 +20,49 @@ func _ready() -> void:
 # IMPORTANT CODE: Initiate chunks to align to main seed with offset via position.
 func _initiateSpaceSector() -> void:
 	# IGNORE: Debug
-	print("Initializing Space Data...")
-	spaceSectorSeed = lib.generateRandomNumber(
-						# Refer to "lib.gd" -> CONFIGURATION section for values.
-						lib.spaceMinimumSpawnSeed,
-						lib.spaceMaximumSpawnSeed,
-						# IGNORE this section, part of number generation.
-						# Refer to "lib.gd" -> "Global Generators" section -> "generateRandomNumber() method".
-						"int", true 
-					)
+	print("Initializing Space Sector Key...")
 	
-	# Create 3 colors for clouds with a loop.
-	for _i in range(3):
-		spaceSectorGases.append(
-							lib.generateRandomColor(
-								# Refer to "lib.gd" -> CONFIGURATION section for values.
-								lib.spaceGasesMinimumColorValue,
-								lib.spaceGasesMaximumColorValue,
-							)
+	# Creates the seed for the galaxy.
+	# Manages if you want a seed with presets.
+	if _spaceSectorRandomGeneration:
+		_spaceSectorSeed = lib.generateRandomNumber(
+							# Refer to "lib.gd" -> CONFIGURATION section for values.
+							lib.spaceMinimumSpawnSeed,
+							lib.spaceMaximumSpawnSeed,
+							# IGNORE this section, part of number generation.
+							# Refer to "lib.gd" -> "Global Generators" section -> "generateRandomNumber() method".
+							"int", true 
 						)
+		
+		# Creates the background color accent of the space:
+		_spaceSectorBackground = lib.generateRandomColor()
 	
-	# Generate what kind of phenomena will be seen at the feature space.
-	spaceSectorPhenomena = lib.generateRandomNumber(1, 2, "int", false) 
+		# Create 3 colors for clouds with a loop.
+		for _spaceSectorGasesIteration in range(3):
+			if lib.generateRandomNumber(0, 1) == 1:
+				_spaceSectorGases.append(lib.generateRandomColor())
+			else:
+				_spaceSectorGases.append("00000000")
+		
+		# Generate what kind of phenomena will be seen at the feature space.
+		_spaceSectorPhenomena = lib.generateRandomNumber(1, 2) 
 	
-	# IGNORE: Debug
-	print("Initialized Space Data! (Seed, Color, Color1, Color2, Phenomena type): " + str(spaceSectorSeed) + ", " + str(spaceSectorGases[0]) + ", " + str(spaceSectorGases[1]) + ", " + str(spaceSectorGases[2])+ ", " + str(spaceSectorPhenomena))
+	else:
+		# Assign chunks of the preset number to the parameters. 
+		var spaceSectorKey: String = "[|-1421538792342290432|e874a580|b5321d80|b2cf5380|3ccb9980|2|]"
+		_spaceSectorSeed = int(spaceSectorKey.get_slice("|", 1))
+		_spaceSectorBackground = (spaceSectorKey.get_slice("|", 2))
+		_spaceSectorGases.append(spaceSectorKey.get_slice("|", 3))
+		_spaceSectorGases.append(spaceSectorKey.get_slice("|", 4))
+		_spaceSectorGases.append(spaceSectorKey.get_slice("|", 5))
+		_spaceSectorPhenomena = int(spaceSectorKey.get_slice("|", 6))
 	
+	# Wait for the scene to be created.
+	await get_tree().create_timer(2).timeout
 	# Proceed to generation of space.
-	spaceBackgroundGenerator.initiateThreadForLoading([spaceSectorSeed, spaceSectorGases[0], spaceSectorGases[1], spaceSectorGases[2]])
+	_spaceBackgroundGenerator.initiateThreadForLoading([[_spaceSectorSeed, _spaceSectorBackground, _spaceSectorGases[0], _spaceSectorGases[1], _spaceSectorGases[2], _spaceSectorPhenomena]])
 	
 	# Proceed to generation of space phenomena.
-	spacePhenomenaGenerator.initiateThreadForLoading([spaceSectorSeed, spaceSectorGases[0], spaceSectorPhenomena])
+	_spacePhenomenaGenerator.initiateThreadForLoading([_spaceSectorSeed, _spaceSectorGases[0], _spaceSectorPhenomena])
 
 #------------------------------------------------------------------------------#
